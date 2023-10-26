2ba169f1145188002159fbb06b3e4a6697e2df82
jotaeme890 <permanentjavi@gmail.com>
2023-10-26 19:36:31 +0200

    Players-about added and menu fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule,],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
